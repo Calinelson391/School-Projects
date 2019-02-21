3b63c8bf692e4d059cef5596620f4530656e739d
Calista Nelson <calistan@usc.edu>
2019-02-21 14:12:07 -0800

    Game File Uploaded

---
+++ game.cpp
@@ -0,0 +1,33 @@
+/*
+	CSCI 270 Spring 2019
+	Programming Assignment
+	Name    :
+	USC ID  :
+	Username:
+	Email   :
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+//	Feel free to include any other C++ standard library if necessary.
+
+using namespace std;
+//	You can add any custom classes / helper functions here.
+
+/*
+	This function receives two parameters n, m, as described in problem description, and
+	it should return the minimum number of times Sora has to try in the worst case,
+	without risking failing too many times.
+*/
+int solve(int n, int m) {
+	return 0;
+}
+
+//	The main function reads the input and outputs your answer.
+//	DO NOT EDIT
+int main() {
+	int n, m;
+	cin >> n >> m;
+	cout << solve(n, m) << endl;
+	return 0;
+}
